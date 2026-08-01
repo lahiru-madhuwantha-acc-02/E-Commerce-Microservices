13cc86a5c1c96c8a0e1ae21cca3deca41bee8ab6
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2026-08-01 23:39:46 +0530

    Init project

---
+++ src/main/java/com/sl2r/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.sl2r.eurekadiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sl2r/eurekadiscoveryserver/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl2r.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
