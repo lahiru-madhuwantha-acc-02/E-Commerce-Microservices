3749f2d4be73f432704cd94a522a2988397b414f
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2026-08-01 23:46:40 +0530

    init project file

---
+++ src/test/java/com/sl2r/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl2r.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
