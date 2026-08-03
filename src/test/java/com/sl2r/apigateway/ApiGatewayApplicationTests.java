143a60c8c39c51f44ff69c162eff8902325b0568
Lahiru-Madhuwantha <lahirumadhuwantha293@gmail.com>
2026-08-03 13:35:31 +0530

    init api gateway

---
+++ src/test/java/com/sl2r/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl2r.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
